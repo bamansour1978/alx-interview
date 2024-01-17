de3471a7e1089cc25d2a2f78a876d29f655d5a90
bamansour1978 <karatman@gmail.com>
2024-01-17 21:55:52 +0100

    0. Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,5 +4,11 @@
 
 def minOperations(n: int) -> int:
     """Function documentation"""
-    ---------------------------
-    return
+    process = 2
+    op = 0
+    while n > 1:
+        while n % process == 0:
+            op += process
+            n /= process
+        process += 1
+    return op
